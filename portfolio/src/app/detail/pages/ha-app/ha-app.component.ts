bc4e2fd14788e5fd130087d764d50e9308aadbbc
JClingo <josh@databasecg.com>
2017-01-16 22:30:19 -0700

    Finished out most of the content

--- portfolio/src/app/detail/pages/ha-app/ha-app.component.ts
+++ portfolio/src/app/detail/pages/ha-app/ha-app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 
+enum pages {
+    home = 0,
+    messaging
+}
+
 @Component({
   selector: 'p-ha-app',
   templateUrl: './ha-app.component.html'
 })
 export class HAAppComponent {
-    
+  page: number = pages.home;
+
+  select(num: number) {
+    if (this.page !== num) {
+      this.page = num;
+    }
+  }
+
+  isSelected(num: number): boolean {
+    return this.page === num;
+  }
 }
--- portfolio/src/app/detail/pages/ha-portal/ha-portal.component.ts
+++ portfolio/src/app/detail/pages/ha-portal/ha-portal.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 
+enum pages {
+    triage = 0,
+    messaging,
+    connection,
+    clinics
+}
+
 @Component({
   selector: 'p-ha-portal',
   templateUrl: './ha-portal.component.html'
 })
 export class HAPortalComponent {
-    
+  page: number = pages.triage;
+
+  select(num: number) {
+    if (this.page !== num) {
+      this.page = num;
+    }
+  }
+
+  isSelected(num: number): boolean {
+    return this.page === num;
+  }
 }
